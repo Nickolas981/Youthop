b6b0efbf5d953ff544ee82bb85157ef9720dd9d9
Nickolas Humeniuk <kolin98111@gmail.com>
2018-02-14 00:45:37 +0200

    Added bottom nav bar

--- app/src/main/java/com/dongumen/nickolas/youthop/activities/MainActivity.java
+++ app/src/main/java/com/dongumen/nickolas/youthop/activities/MainActivity.java
@@ -2,6 +2,8 @@ package com.dongumen.nickolas.youthop.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -11,9 +13,14 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import com.dongumen.nickolas.youthop.R;
+import com.dongumen.nickolas.youthop.fragments.HomeFragment;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener,
+                        HomeFragment.OnFragmentInteractionListener{
+
+    Integer container = R.id.container;
+    HomeFragment homeFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,8 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        homeFragment = HomeFragment.newInstance();
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
 
@@ -33,6 +42,7 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         navigationView.getMenu().getItem(0).setChecked(true);
+        changeFragment(homeFragment);
     }
 
     @Override
@@ -65,8 +75,16 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
+
+        switch (id){
+            case R.id.nav_home: {
+                changeFragment(homeFragment);
+            }
+            default:{
+                //TODO: ??
+            }
+        }
 
 //        if (id == R.id.nav_camera) {
 //            // Handle the camera action
@@ -85,5 +103,17 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void changeFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    @Override
+    public void onFragmentInteraction() {
+
     }
 }

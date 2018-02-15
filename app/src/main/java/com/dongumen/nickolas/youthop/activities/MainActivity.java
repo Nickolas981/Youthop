c1dfb9549673608e70c7ac0d7945e3912c747404
Nickolas Humeniuk <kolin98111@gmail.com>
2018-02-15 17:11:36 +0200

    Fixed orientation

--- app/src/main/java/com/dongumen/nickolas/youthop/activities/MainActivity.java
+++ app/src/main/java/com/dongumen/nickolas/youthop/activities/MainActivity.java
@@ -1,5 +1,6 @@
 package com.dongumen.nickolas.youthop.activities;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
@@ -28,6 +29,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 

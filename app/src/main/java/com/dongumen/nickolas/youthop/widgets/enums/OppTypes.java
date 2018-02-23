57017dee11a1c948cfa928fecddde88c641a7aa4
Nickolas Humeniuk <kolin98111@gmail.com>
2018-02-23 13:23:20 +0200

    Added oppType enum

---
+++ app/src/main/java/com/dongumen/nickolas/youthop/widgets/enums/OppTypes.java
@@ -0,0 +1,12 @@
+package com.dongumen.nickolas.youthop.widgets.enums;
+
+public enum OppTypes {
+    Scholarships,
+    Fellowships,
+    Jobs,
+    Internships,
+    Conferences,
+    Competitions,
+    Grants,
+    Miscellaneous
+}
